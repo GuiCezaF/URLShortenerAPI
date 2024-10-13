be2ca1013700c02b5a41e5002da70edf3b84b844
Guilherme.Felipe <guilherme.felipe@retta.com.br>
2024-10-13 20:29:58 -0300

    feat: adicionando controllers de criação da url encurtada e de redirecionar a pessoa para o link

---
+++ src/controllers/urlController.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from 'express';
+import { logger } from '../utils/logger';
+import { UrlService } from '../services/urlService';
+
+const urlService = new UrlService();
+
+export class UrlController {
+  async shortenUrl(req: Request, res: Response): Promise<void> {
+    const { original_url } = req.body;
+
+    if (!original_url) {
+      res.status(400).json({ message: 'URL is required' });
+      return;
+    }
+
+    try {
+      const shortenedUrl = await urlService.createShortUrl(
+        original_url,
+        `${req.protocol}://${req.get('host')}`,
+      );
+      logger.info('Url criada', shortenedUrl);
+      res.status(201).json({ shortenedUrl });
+    } catch (err) {
+      logger.error(err);
+      res.status(500).json({ message: 'Error creating short URL' });
+    }
+  }
+
+  async redirectToOriginal(req: Request, res: Response): Promise<void> {
+    const { hash } = req.params;
+
+    try {
+      const originalUrl = await urlService.getOriginalUrl(hash);
+      if (originalUrl) {
+        res.redirect(originalUrl);
+        logger.info('Redirecionando para URL original', originalUrl);
+      } else {
+        res.status(404).json({ message: 'URL not found' });
+      }
+    } catch (err) {
+      logger.error(err);
+      res.status(500).json({ message: 'Error retrieving URL' });
+    }
+  }
+}
