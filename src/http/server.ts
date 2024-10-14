836b2fd01d888311d47f5e3017b921fda5df029d
Guilherme.Felipe <guilherme.felipe@retta.com.br>
2024-10-14 09:53:33 -0300

    feat: liberando o cors para todos

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import { env } from '../env';
 import router from '../routes/urlRoutes';
 import { logger } from '../utils/logger';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+// TODO(guilherme): configurar o cors para aceitar apenas POST do front e apenas GET do navegador
 
 app.use('/', router);
 
